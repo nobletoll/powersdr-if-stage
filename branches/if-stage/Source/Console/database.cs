4b0d236c33ef6efb35e9d5fb8527f8ee55e46425
cgatesman <cgatesman@00a3481c-4c4a-0410-8558-3fbba560ec4d>
2010-02-13 21:47:08 +0000

    Issue 123: Copy European database when we detect commas are used for decimal separator.

--- branches/if-stage/Source/Console/database.cs
+++ branches/if-stage/Source/Console/database.cs
@@ -1387,8 +1387,14 @@ namespace PowerSDR
 			// :W1CEG: PowerSDR IF-Stage will copy a default database from
 			//         the install bin directory instead of going through
 			//         the setup wizard.
+			string separator = System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+			string dbFile = "\\database.xml";
+
+			if (separator.Equals(","))
+				dbFile = "\\database-europe.xml";
+
 			if (!File.Exists(app_data_path + "\\database.xml"))
-				File.Copy(Application.StartupPath + "\\database.xml",
+				File.Copy(Application.StartupPath + dbFile,
 					app_data_path + "\\database.xml");
 
 			ds.ReadXml(app_data_path + "\\database.xml");
